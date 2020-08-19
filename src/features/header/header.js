48be18eef4894dabe9f1fc1a2b5224ea611f21da
Derek Etman <dereketman@gmail.com>
2020-08-19 10:49:40 -0700

    Feat: Header + Footer

--- src/features/header/header.js
+++ src/features/header/header.js
@@ -1,14 +1,17 @@
-import { Grid, IconButton } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
+
+import { Grid, IconButton, SvgIcon, Link, Input } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import SearchIcon from "@material-ui/icons/Search";
 import "./header.scss";
 import logo_img from "./header_assets/font_logo_color.png";
 
 export default function Header() {
 	return (
-		<Grid container className="header_container">
-			<Grid item lg={5}>
-				<IconButton className="header_menu_button">
+		<Grid container className="header_container" spacing={0}>
+			<Grid item lg={4} className="header_left_container">
+				<IconButton className="header_menu_button" edge="start">
 					<MenuIcon fontSize="large" />
 				</IconButton>
 				<img
@@ -18,7 +21,19 @@ export default function Header() {
 				/>
 			</Grid>
 
-			<Grid item className="header_nav_container" lg={4}></Grid>
+			<Grid item lg={5} className="header_searchbar_container">
+					<Input autoFocus="true" placeholder="Search" fullWidth="true" />
+					<SvgIcon>
+						<SearchIcon />
+					</SvgIcon>
+			</Grid>
+
+			<Grid item className="header_right_container" lg={3}>
+				<Link className="header_right_element"> Log In</Link>
+				<SvgIcon className="header_right_element">
+					<ShoppingCartIcon />
+				</SvgIcon>
+			</Grid>
 		</Grid>
 	);
 }
