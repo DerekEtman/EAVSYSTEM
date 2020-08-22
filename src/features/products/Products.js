d9de2ff73cca2f598377aecafd7a8ca1fae6a04e
Derek Etman <dereketman@gmail.com>
2020-08-22 00:16:31 -0700

    Feat: Product List Layout Responsive

--- src/features/products/Products.js
+++ src/features/products/Products.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./products.scss";
+import { Container, Box, Grid, Hidden } from "@material-ui/core";
 
 export default function Products() {
-    return(
-        <>
-        hi
-        </>
-    )
+	return (
+		<Grid container className="product_container" spacing={3}>
+			<Hidden smDown>
+				<Grid item className="product_left_box" md={2}>
+					{" "}
+					TEST ONE
+				</Grid>
+			</Hidden>
+			<Grid item className="product_right_box" md={10} sm={12}>
+				<Grid container spacing={3} wrap="wrap">
+					<Grid item xs={12} className="product_grid_item">
+						<h1>Box1</h1>
+					</Grid>
+					<Grid item xs={12} className="product_grid_item">
+						<h1>Box2</h1>
+					</Grid>
+					<Grid item xs={12} className="product_grid_item">
+						<h1>Box3</h1>
+					</Grid>
+					<Grid item xs={12} className="product_grid_item" s>
+						<h1>Box4</h1>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
