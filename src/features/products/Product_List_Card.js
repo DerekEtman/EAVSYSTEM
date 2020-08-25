f780c65d3bd08ad2802a36de1dde24ed55a7dc41
Derek Etman <dereketman@gmail.com>
2020-08-25 15:21:32 -0700

    feat: prod_details

--- src/features/products/Product_List_Card.js
+++ src/features/products/Product_List_Card.js
@@ -1,20 +1,20 @@
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import img_holder from "./products_assets/no_image_holder.png";
 
 export default function Product_List_Card() {
 	return (
-		<div className="product_list_card">
-			<Grid container >
+			<Grid container className="product_list_card" spacing={3}>
 				<Grid item className="product_list_image_container" xs={12} md={3} >
-					<img src={img_holder} className="product_list_image" style={{maxWidth:"60%"}} alt="Product placeholder img"/>
+					<img src={img_holder} className="product_list_image" alt="Product placeholder img"/>
 				</Grid>
 				<Grid className="product_list_info"  item md={9} xs={12}>
-					<h2> Item Name</h2>
-					<h3>Price of Item</h3>
-					<Button variant="contained">Add To Cart</Button>
+					<Typography variant="h4" gutterBottom="true" className="product_list_info_element"> Item Name</Typography>
+                    <Typography variant="body1" gutterBottom="true" className="product_list_info_element"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</Typography>
+					<Typography className="product_list_info_element"  color="secondary" >Price of Item</Typography>
+					<Button variant="contained" size="large"  className="product_list_info_element" >Add To Cart</Button>
 				</Grid>
 			</Grid>
-		</div>
+
 	);
 }
